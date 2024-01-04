bc44634056e5e4db708d370c486441d5069b9d6c
morispolanco <morispolanco@gmail.com>
2024-01-04 08:32:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,37 @@
 import streamlit as st
 import requests
 
-def obtener_respuesta_pregunta(pregunta, api_token):
-    url = "https://api.botsonic.ai/v1/botsonic/generate"
-    headers = {
-        "Authorization": f"Bearer {api_token}",
-        "Content-Type": "application/json",
-    }
-    payload = {"input": pregunta}
+def obtener_respuesta_pregunta(pregunta, botsonic_token):
+    api_endpoint = 'https://api.botsonic.ai/v1/botsonic/generate'
 
-    response = requests.post(url, json=payload, headers=headers)
+    headers = {
+        'User-Agent': 'python-requests/2.28.1',
+        'accept': 'application/json',
+        'Authorization': f'Bearer {botsonic_token}',
+    }
+
+    json_data = {
+        'question': pregunta,
+        'chat_history': [],
+    }
+
+    response = requests.post(api_endpoint, headers=headers, json=json_data)
 
     if response.status_code == 200:
-        return response.json().get("output", "No se pudo obtener una respuesta.")
+        return response.json().get('output', 'No se pudo obtener una respuesta.')
     else:
-        return f"Error al realizar la solicitud: {response.status_code}"
+        return f'Error al realizar la solicitud: {response.status_code}'
 
 def main():
     st.title("Aplicación de Streamlit - Preguntas sobre leyes de Guatemala")
 
     pregunta_usuario = st.text_input("Ingrese su pregunta:")
 
+    # Reemplaza '<Your Botsonic token>' con tu token real de Botsonic
+    botsonic_token = '9e9f56f9-5004-4e78-a875-2a28b320c8e9'
+
     if st.button("Obtener respuesta"):
-        api_token = "9e9f56f9-5004-4e78-a875-2a28b320c8e9"
-        respuesta = obtener_respuesta_pregunta(pregunta_usuario, api_token)
+        respuesta = obtener_respuesta_pregunta(pregunta_usuario, botsonic_token)
         st.write("Respuesta:", respuesta)
 
 if __name__ == "__main__":
