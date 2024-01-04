7f56d1f35c6fe56a8749ebb308c9009475d4202e
morispolanco <morispolanco@gmail.com>
2024-01-04 09:44:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,12 @@ import streamlit as st
 import requests
 
 def obtener_respuesta_pregunta(pregunta, botsonic_token):
-    api_endpoint = '<your-API-endpoint>'  # Reemplaza con la URL real de la API
+    api_endpoint = 'https://api.botsonic.ai/v1/botsonic/generate'
 
     headers = {
-        'Accept-Encoding': 'gzip, deflate',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json',
         'User-Agent': 'python-requests/2.28.1',
         'accept': 'application/json',
-        'token': botsonic_token,
+        'Authorization': f'Bearer {botsonic_token}',
     }
 
     json_data = {
@@ -21,9 +18,9 @@ def obtener_respuesta_pregunta(pregunta, botsonic_token):
     response = requests.post(api_endpoint, headers=headers, json=json_data)
 
     if response.status_code == 200:
-        return response.json().get('output', 'No se pudo obtener una respuesta.')
+        return response.json()[0]['data']['answer'], response.json()[0]['data']['sources']
     else:
-        return f'Error al realizar la solicitud: {response.status_code}\nRespuesta completa: {response.text}'
+        return f'Error al realizar la solicitud: {response.status_code}\nRespuesta completa: {response.text}', None
 
 def main():
     st.title("Aplicación de Streamlit - Preguntas sobre leyes de Guatemala")
@@ -34,8 +31,15 @@ def main():
     botsonic_token = '9e9f56f9-5004-4e78-a875-2a28b320c8e9'
 
     if st.button("Obtener respuesta"):
-        respuesta = obtener_respuesta_pregunta(pregunta_usuario, botsonic_token)
+        respuesta, fuentes = obtener_respuesta_pregunta(pregunta_usuario, botsonic_token)
         st.write("Respuesta:", respuesta)
+
+        if fuentes:
+            st.write("Fuentes:")
+            for fuente in fuentes:
+                st.write(f"URL: {fuente['url']}")
+                st.write(f"Contenido: {fuente['content']}")
+                st.write("---")
 
 if __name__ == "__main__":
     main()
