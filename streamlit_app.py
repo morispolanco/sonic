21f7f4cf100de74ba50c80425123d5933de4b3ab
morispolanco <morispolanco@gmail.com>
2024-01-04 09:47:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ def obtener_respuesta_pregunta(pregunta, botsonic_token):
     response = requests.post(api_endpoint, headers=headers, json=json_data)
 
     if response.status_code == 200:
-        return response.json()[0]['data']['answer'], response.json()[0]['data']['sources']
+        return response.json()[0]['data']['answer']
     else:
-        return f'Error al realizar la solicitud: {response.status_code}\nRespuesta completa: {response.text}', None
+        return f'Error al realizar la solicitud: {response.status_code}\nRespuesta completa: {response.text}'
 
 def main():
     st.title("Aplicación de Streamlit - Preguntas sobre leyes de Guatemala")
@@ -31,15 +31,8 @@ def main():
     botsonic_token = '9e9f56f9-5004-4e78-a875-2a28b320c8e9'
 
     if st.button("Obtener respuesta"):
-        respuesta, fuentes = obtener_respuesta_pregunta(pregunta_usuario, botsonic_token)
+        respuesta = obtener_respuesta_pregunta(pregunta_usuario, botsonic_token)
         st.write("Respuesta:", respuesta)
-
-        if fuentes:
-            st.write("Fuentes:")
-            for fuente in fuentes:
-                st.write(f"URL: {fuente['url']}")
-                st.write(f"Contenido: {fuente['content']}")
-                st.write("---")
 
 if __name__ == "__main__":
     main()
